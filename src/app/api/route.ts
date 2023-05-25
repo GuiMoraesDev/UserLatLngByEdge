94006f916ba8eb5d8775e501f6ddda40225f56a5
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-25 10:10:56 -0300

    chore: route config

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,21 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
 
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
 export function GET(request: NextRequest) {
-  const headersList = headers();
+  const { geo } = request;
 
-  const referer = headersList.get("referer");
-  const requestHeader = headersList.get("request");
-  const url = headersList.get("url");
+  const lat = geo?.latitude;
+  const lng = geo?.longitude;
 
   return NextResponse.json({
     name: `Hello, from ${request.url} I'm now an Edge Function!`,
-    referer,
-    requestHeader,
-    url,
+    geo,
+    lat,
+    lng,
   });
 }
