c7225482edb646617255e475de0be966890e40e8
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-25 10:15:43 -0300

    feat: add new requests parameters

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,15 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = "edge";
 
 export function GET(request: NextRequest) {
-  const { geo } = request;
-
-  const lat = geo?.latitude;
-  const lng = geo?.longitude;
+  const { geo, destination, ip, credentials, headers } = request;
 
   return NextResponse.json({
     name: `Hello, from ${request.url} I'm now an Edge Function!`,
     geo,
-    lat,
-    lng,
+    destination,
+    ip,
+    credentials,
+    headers,
   });
 }
