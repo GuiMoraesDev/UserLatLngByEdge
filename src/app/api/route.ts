7b0fc3271587fa79c6d83061c742d03881aa06ca
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-25 10:23:22 -0300

    Chore: change API return

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,10 +6,9 @@ export function GET(request: NextRequest) {
   const { geo, ip } = request;
 
   return NextResponse.json({
-    message: `
-    Hey, you
-    I'm a edge function created for global domination and I know where you are.
-    You are talking from ${geo?.city}, ${geo?.region}, ${geo?.country} and I know your IP is ${ip}
-    BTW, that's your location on the map: https://www.google.com/maps/place/${geo?.latitude},${geo?.longitude}`,
+    hello_message: `Hey, you`,
+    presentation: `I'm a edge function created for global domination and I know where you are.`,
+    welcome_message: `You are talking from ${geo?.city}, ${geo?.region}, ${geo?.country} and I know your IP is ${ip}`,
+    see_you_soon_message: `BTW, that's your location on the map: https://www.google.com/maps/place/${geo?.latitude},${geo?.longitude}`,
   });
 }
