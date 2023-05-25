b4cbddeed652bf2162d86a4fbdc6084423cf4851
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-25 10:18:15 -0300

    chore: change headers on request

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,14 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = "edge";
 
 export function GET(request: NextRequest) {
-  const { geo, destination, ip, credentials, headers } = request;
+  const { geo, ip } = request;
 
   return NextResponse.json({
     name: `Hello, from ${request.url} I'm now an Edge Function!`,
     geo,
-    destination,
     ip,
-    credentials,
-    headers,
   });
 }
