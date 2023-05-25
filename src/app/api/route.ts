933ee2b0111941444ef831b31b6f0c9581872505
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-25 10:20:51 -0300

    feat: world domination

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,8 +6,10 @@ export function GET(request: NextRequest) {
   const { geo, ip } = request;
 
   return NextResponse.json({
-    name: `Hello, from ${request.url} I'm now an Edge Function!`,
-    geo,
-    ip,
+    message: `
+    Hey, you
+    I'm a edge function created for global domination and I know where you are.
+    You are talking from ${geo?.city}, ${geo?.region}, ${geo?.country} and I know your IP is ${ip}
+    BTW, that's your location on the map: https://www.google.com/maps/place/${geo?.latitude},${geo?.longitude}`,
   });
 }
