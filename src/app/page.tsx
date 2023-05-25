f2d86b048b00e24bf9b1b3b79496cdca2e68f4e8
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-25 10:53:46 -0300

    feat: add console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ const getData = async (): Promise<Data> => {
 
 export default async function Home() {
   const data = await getData();
+  console.log(data);
   const { city, region, country, ip, latitude, longitude } = data;
 
   return (
@@ -46,9 +47,10 @@ export default async function Home() {
         </p>
 
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          BTW, that&apos;s your location on the map:
+          BTW, that&apos;s your location on the map:{" "}
           <a
             href={`https://www.google.com/maps/place/${latitude},${longitude}`}
+            className="underline text-lg"
           >
             {latitude}, {longitude}
           </a>
