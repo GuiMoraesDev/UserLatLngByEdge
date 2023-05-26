2a063230f733d8a879deeb3ad10adf6588c6f35b
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-26 13:43:48 -0300

    refactor: refactor page to use headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,21 +49,21 @@ export default function Home() {
         </p>
 
         <p className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          You are talking from {cityCookie?.value}, {regionCookie?.value},{" "}
-          {countryCookie?.value}
+          You are talking from {cityHeader}, {regionHeader},{" "}
+          {countryHeader}
         </p>
 
         <p className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          I know your IP is {ipCookie?.value}
+          I know your IP is {ipHeader}
         </p>
 
         <p className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           BTW, that&apos;s your location on the map:{" "}
           <a
-            href={`https://www.google.com/maps/place/${latitudeCookie?.value},${longitudeCookie?.value}`}
+            href={`https://www.google.com/maps/place/${latitudeHeader},${longitudeHeader}`}
             className="underline text-lg"
           >
-            {latitudeCookie?.value}, {longitudeCookie?.value}
+            {latitudeHeader}, {longitudeHeader}
           </a>
         </p>
 
