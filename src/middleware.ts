a61b7c30f9728eed6b6e425c82ec90ddffdf5a22
Guilherme Moraes <guimoraes.dev@gmail.com>
2023-05-26 12:02:14 -0300

    chore: remove async from middleware function

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const { geo } = request;
   const ip = request.ip || "Unknown";
   const city = geo?.city || "Unknown";
